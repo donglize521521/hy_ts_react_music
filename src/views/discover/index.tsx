308f6d56c2ff7ee8d2da99af42f923ffbe156683
星霜下逆天生 <1486252015@qq.com>
2023-09-09 12:26:55 +0800

    配置路由router(一)

---
+++ src/views/discover/index.tsx
@@ -0,0 +1,10 @@
+//【注意】
+// ①只要文件中用到jsx语法, 那该文件就应是.tsx。
+// ②只要是.tsx文件都要引入import React from 'react',没用到也需要引入,否则jsx语法会报错
+import React from 'react'
+
+const Discover = () => {
+  return <div>discover</div>
+}
+
+export default Discover
