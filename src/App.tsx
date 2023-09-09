28cb50cd31390135032124387694cbc43742c12d
星霜下逆天生 <1486252015@qq.com>
2023-09-09 23:28:39 +0800

    集成redux和useSelector的使用

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 import React, { Suspense } from 'react'
 import { useRoutes, Link } from 'react-router-dom'
+import { shallowEqual } from 'react-redux'
 import routes from './router'
+import { useAppSelector } from './store'
+// import { IRootState } from './store'
+
+// import store from './store'
+
+// type GetStateFnType = typeof store.getState
+// type IRootState = ReturnType<GetStateFnType>
 
 function App() {
+  // const { count, message } = useSelector(
+  //   (state: IRootState) => ({
+  //     count: state.counter.count,
+  //     message: state.counter.message
+  //   })
+  //   // shallowEqual
+  // )
+  const { count, message } = useAppSelector(
+    (state) => ({
+      count: state.counter.count,
+      message: state.counter.message
+    }),
+    shallowEqual
+  )
   return (
     <div className="App">
       <div className="nav">
@@ -11,6 +33,8 @@ function App() {
         <Link to="/focus">关注</Link>
         <Link to="/download">下载客户端</Link>
       </div>
+      <h2>当前计数: {count}</h2>
+      <h2>当前消息: {message}</h2>
       <Suspense fallback="loading....">
         <div className="main">{useRoutes(routes)}</div>
       </Suspense>
---
+++ src/store/index.ts
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { useSelector, TypedUseSelectorHook } from 'react-redux'
+
+import counterReducer from './modules/counter'
+
+const store = configureStore({
+  reducer: {
+    counter: counterReducer
+  }
+})
+type GetStateFnType = typeof store.getState
+export type IRootState = ReturnType<GetStateFnType>
+
+// useAppSelector的hook
+export const useAppSelector: TypedUseSelectorHook<IRootState> = useSelector
+
+export default store
