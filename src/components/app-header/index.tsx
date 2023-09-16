f92939bef163156bdd777d8103ace12f740065a0
星霜下逆天生 <1486252015@qq.com>
2023-09-16 16:59:59 +0800

    样式复用的两种方式

--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -1,6 +1,7 @@
 import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 import { Link } from 'react-router-dom'
+import { HeaderWrapper } from './style'
 
 interface IProps {
   children?: ReactNode
@@ -8,14 +9,14 @@ interface IProps {
 
 const AppHeader: FC<IProps> = () => {
   return (
-    <div>
-      <div className="nav">
+    <HeaderWrapper>
+      <div className="content wrap-v1">
         <Link to="/discover">发现音乐</Link>
         <Link to="/mine">我的音乐</Link>
         <Link to="/focus">关注</Link>
         <Link to="/download">下载客户端</Link>
       </div>
-    </div>
+    </HeaderWrapper>
   )
 }
 export default memo(AppHeader)
