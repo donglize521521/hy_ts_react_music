78d64ea4e912f348d70fba72572186abcbbc9109
星霜下逆天生 <1486252015@qq.com>
2023-09-16 23:25:22 +0800

    Header中的左侧列表展示

--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -1,20 +1,44 @@
 import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 import { Link } from 'react-router-dom'
-import { HeaderWrapper } from './style'
+import { HeaderWrapper, HeaderLeft, HeaderRight } from './style'
+
+import headerTitles from '@/assets/data/header-titles.json'
 
 interface IProps {
   children?: ReactNode
 }
 
 const AppHeader: FC<IProps> = () => {
+  function showItem(item: any) {
+    if (item.type === 'path') {
+      return <Link to={item.link}>{item.title}</Link>
+    } else {
+      return (
+        <a href={item.link} rel="noreferrer" target="_blank">
+          {item.title}
+        </a>
+      )
+    }
+  }
   return (
     <HeaderWrapper>
       <div className="content wrap-v1">
-        <Link to="/discover">发现音乐</Link>
-        <Link to="/mine">我的音乐</Link>
-        <Link to="/focus">关注</Link>
-        <Link to="/download">下载客户端</Link>
+        <HeaderLeft>
+          <a className="logo sprite_01 " href="/#">
+            网易云音乐
+          </a>
+          <div className="title-list">
+            {headerTitles.map((item) => {
+              return (
+                <div className="item" key={item.title}>
+                  {showItem(item)}
+                </div>
+              )
+            })}
+          </div>
+        </HeaderLeft>
+        <HeaderRight>HeaderRight</HeaderRight>
       </div>
     </HeaderWrapper>
   )
