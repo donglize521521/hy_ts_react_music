08184f197027e0984270ef8f194031908536a0c2
星霜下逆天生 <1486252015@qq.com>
2023-09-17 21:18:45 +0800

    发现页面的二级菜单开发

---
+++ src/views/discover/c-cpns/nav-bar/index.tsx
@@ -0,0 +1,26 @@
+import React, { memo } from 'react'
+import type { FC, ReactNode } from 'react'
+import { NavLink } from 'react-router-dom'
+import { NavWrapper } from './style'
+import { dicoverMenu } from '@/assets/data/local_data'
+
+interface IProps {
+  children?: ReactNode
+}
+
+const NavBar: FC<IProps> = () => {
+  return (
+    <NavWrapper>
+      <div className="wrap-v1 nav">
+        {dicoverMenu.map((item) => {
+          return (
+            <div className="item" key={item.link}>
+              <NavLink to={item.link}>{item.title}</NavLink>
+            </div>
+          )
+        })}
+      </div>
+    </NavWrapper>
+  )
+}
+export default memo(NavBar)
