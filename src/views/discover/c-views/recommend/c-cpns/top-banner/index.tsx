5f7d7b4ec39ac679990dca3e63f51324107afc25
星霜下逆天生 <1486252015@qq.com>
2023-09-21 17:26:00 +0800

    实现轮播图点击左右切换效果

--- src/views/discover/c-views/recommend/c-cpns/top-banner/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/top-banner/index.tsx
@@ -1,6 +1,6 @@
 import { useAppSelector } from '@/store'
-import React, { memo } from 'react'
-import type { FC, ReactNode } from 'react'
+import React, { memo, useRef } from 'react'
+import type { ElementRef, FC, ReactNode } from 'react'
 import { shallowEqual } from 'react-redux'
 import { Carousel } from 'antd'
 import { BannerWrapper, BannerLeft, BannerRight, BannerControl } from './style'
@@ -10,18 +10,27 @@ interface IProps {
 }
 
 const TopBanner: FC<IProps> = () => {
-  /**从store中获取数据*/
+  /*定义内部的数据*/
+  const bannerRef = useRef<ElementRef<typeof Carousel>>(null)
+  /*从store中获取数据*/
   const { banners } = useAppSelector(
     (state) => ({
       banners: state.recommend.banners
     }),
     shallowEqual
   )
+  /**数据处理函数**/
+  function handlePrevClick() {
+    bannerRef.current?.prev()
+  }
+  function handleNextClick() {
+    bannerRef.current?.next()
+  }
   return (
     <BannerWrapper>
       <div className="banner wrap-v2">
         <BannerLeft>
-          <Carousel autoplay autoplaySpeed={3000}>
+          <Carousel autoplay autoplaySpeed={3000} ref={bannerRef}>
             {banners.map((item) => {
               return (
                 <div className="banner-item" key={item.imageUrl}>
@@ -37,8 +46,8 @@ const TopBanner: FC<IProps> = () => {
         </BannerLeft>
         <BannerRight></BannerRight>
         <BannerControl>
-          <button className="btn left" />
-          <button className="btn right" />
+          <button className="btn left" onClick={handlePrevClick} />
+          <button className="btn right" onClick={handleNextClick} />
         </BannerControl>
       </div>
     </BannerWrapper>
