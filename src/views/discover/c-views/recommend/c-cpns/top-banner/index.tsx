336f63b8141b0e26a2eb6df660700c0b2763303d
星霜下逆天生 <1486252015@qq.com>
2023-09-21 16:52:40 +0800

    实现轮播图

--- src/views/discover/c-views/recommend/c-cpns/top-banner/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/top-banner/index.tsx
@@ -2,6 +2,8 @@ import { useAppSelector } from '@/store'
 import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 import { shallowEqual } from 'react-redux'
+import { Carousel } from 'antd'
+import { BannerWrapper, BannerLeft, BannerRight, BannerControl } from './style'
 
 interface IProps {
   children?: ReactNode
@@ -16,11 +18,30 @@ const TopBanner: FC<IProps> = () => {
     shallowEqual
   )
   return (
-    <div>
-      {banners.map((item) => {
-        return <div key={item.imageUrl}>{item.imageUrl}</div>
-      })}
-    </div>
+    <BannerWrapper>
+      <div className="banner wrap-v2">
+        <BannerLeft>
+          <Carousel autoplay autoplaySpeed={3000}>
+            {banners.map((item) => {
+              return (
+                <div className="banner-item" key={item.imageUrl}>
+                  <img
+                    className="image"
+                    src={item.imageUrl}
+                    alt={item.typeTitle}
+                  />
+                </div>
+              )
+            })}
+          </Carousel>
+        </BannerLeft>
+        <BannerRight></BannerRight>
+        <BannerControl>
+          <button className="btn left" />
+          <button className="btn right" />
+        </BannerControl>
+      </div>
+    </BannerWrapper>
   )
 }
 export default memo(TopBanner)
